4f231e6801c924218e3aab0e6c62611b373453f3
JRice <jrice@eol.org>
2016-08-24 13:49:49 -0400

    Less querying:

--- app/models/data_object.rb
+++ app/models/data_object.rb
@@ -19,10 +19,10 @@ class DataObject < ActiveRecord::Base
 
     def restore_inat
       count = 0
-      last_id = 21621252
+      last_id = 21621252 # I looked. This was the last ID before missing-iNat.
       images = bad_inat_images.where(["id > ?", last_id]).order(:id).limit(50)
       logger.warn "Started #restore_inat"
-      while images.size > 0
+      while images.exists?
         images.each do |image|
           image.restore
           count += 1
